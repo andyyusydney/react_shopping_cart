42b7162baaf19a56bdf6ab4cfc0415fce0074e3a
FOXTEL\zangli <lin.zang@foxtel.com.au>
2017-03-14 08:53:50 +1100

    PRF-17: Update sign up pages

--- src/main/webapp/src/js/form-progress-bar.js
+++ src/main/webapp/src/js/form-progress-bar.js
@@ -36,4 +36,44 @@ $(document).ready(function(){
 
        }
   });
+
+  //detect data-default-step and set status value when page load
+  var dataDefaultStep = $('.progress-bar-cont').attr('data-default-step');
+
+  if(typeof dataDefaultStep != 'undefined' && dataDefaultStep != null && dataDefaultStep != ''){console.log(dataDefaultStep);
+
+    switch(dataDefaultStep) {
+      case '1':
+        $('#progress-bar').val('0');
+        $('.progress-bar-cont').find('div.first').addClass('border-change');
+        break;
+      case '2':
+        $('#progress-bar').val('34');
+        $('.progress-bar-cont').find('div.first').addClass('border-change');
+        $('.progress-bar-cont').find('div.second').addClass('border-change');
+        break;
+      case '3':
+        $('#progress-bar').val('67');
+        $('.progress-bar-cont').find('div.first').addClass('border-change');
+        $('.progress-bar-cont').find('div.second').addClass('border-change');
+        $('.progress-bar-cont').find('div.third').addClass('border-change');
+        break;
+      case '4':
+        $('#progress-bar').val('100');
+        $('.progress-bar-cont').find('div.first').addClass('border-change');
+        $('.progress-bar-cont').find('div.second').addClass('border-change');
+        $('.progress-bar-cont').find('div.third').addClass('border-change');
+        $('.progress-bar-cont').find('div.fourth').addClass('border-change');
+        break;
+      default:
+        $('#progress-bar').val('34');
+        $('.progress-bar-cont').find('div.first').addClass('border-change');
+        $('.progress-bar-cont').find('div.second').addClass('border-change');
+        break;  
+    }
+  }else{
+    $('#progress-bar').val('34');
+    $('.progress-bar-cont').find('div.first').addClass('border-change');
+    $('.progress-bar-cont').find('div.second').addClass('border-change');
+  }
 });
