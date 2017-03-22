7fd13ade399aecb8cf180960f1e51d35db883ff0
FOXTEL\zangli <lin.zang@foxtel.com.au>
2017-03-22 17:23:23 +1100

    PRF-26: Update foxtel-now-album variable name

--- src/main/webapp/src/js/shop.js
+++ src/main/webapp/src/js/shop.js
@@ -2,12 +2,12 @@
 
 $(document).ready(function(){
 	if($('.foxtel-now-album')){
-		$foxtel-now-album = $('.foxtel-now-album');
-		$foxtel-now-album.find('.foxtel-now-btn.enable').on('click',function(){
+		$foxtel_now_album = $('.foxtel-now-album');
+		$foxtel_now_album.find('.foxtel-now-btn.enable').on('click',function(){
 			$(this).addClass('hidden');
 			$(this).siblings('.foxtel-now-btn.disabled').removeClass('hidden');
 		})
-		$foxtel-now-album.find('.foxtel-now-btn').on('click',function(e){
+		$foxtel_now_album.find('.foxtel-now-btn').on('click',function(e){
 			e.stopImmediatePropagation();
             e.preventDefault();
             return false;
