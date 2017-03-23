ae154f52f2caaa747194d0e9e481868f58c1ce84
FOXTEL\zangli <lin.zang@foxtel.com.au>
2017-03-23 16:34:01 +1100

    PRF-124: Update status-bar styles

--- src/main/webapp/src/js/form-progress-bar.js
+++ src/main/webapp/src/js/form-progress-bar.js
@@ -10,32 +10,32 @@ $(document).ready(function(){
     return;
   }
 
-  //set default value
-  $progressBarComp.on('click','.foxtel-now-progress-bar-dot-wrapper',function(){
-    $this = $(this);
-    var dataLink = $this.data('status-link');
+  // //set default value
+  // $progressBarComp.on('click','.foxtel-now-progress-bar-dot-wrapper',function(){
+  //   $this = $(this);
+  //   var dataLink = $this.data('status-link');
 
-    if ($(this).hasClass('first')){
+  //   if ($(this).hasClass('first')){
 
-        $('#progress-bar').val('0');
-        $(this).nextAll().removeClass('border-change');
-       }else if ($(this).hasClass('second')){
-        $(this).nextAll().removeClass('border-change');  
-        $('#progress-bar').val('34');
-        $(this).prevAll().addClass('border-change');  
-        $(this).addClass('border-change');
-       }else if ($(this).hasClass('third')){
-        $(this).nextAll().removeClass('border-change');  
-        $('#progress-bar').val('67');
-        $(this).prevAll().addClass('border-change'); 
-        $(this).addClass('border-change');
-       } else{
-         $('#progress-bar').val('100');
-         $(this).addClass('border-change');
-        $(this).prevAll().addClass('border-change');
+  //       $('#progress-bar').val('0');
+  //       $(this).nextAll().removeClass('border-change');
+  //      }else if ($(this).hasClass('second')){
+  //       $(this).nextAll().removeClass('border-change');  
+  //       $('#progress-bar').val('34');
+  //       $(this).prevAll().addClass('border-change');  
+  //       $(this).addClass('border-change');
+  //      }else if ($(this).hasClass('third')){
+  //       $(this).nextAll().removeClass('border-change');  
+  //       $('#progress-bar').val('67');
+  //       $(this).prevAll().addClass('border-change'); 
+  //       $(this).addClass('border-change');
+  //      } else{
+  //        $('#progress-bar').val('100');
+  //        $(this).addClass('border-change');
+  //       $(this).prevAll().addClass('border-change');
 
-       }
-  });
+  //      }
+  // });
 
   //detect data-default-step and set status value when page load
   var dataDefaultStep = $progressBarComp.attr('data-default-step');
@@ -45,35 +45,35 @@ $(document).ready(function(){
     switch(dataDefaultStep) {
       case '1':
         $('#progress-bar').val('0');
-        $progressBarComp.find('div.first').addClass('border-change');
+        $progressBarComp.find('div.first').addClass('border-change').find('p').css({'font-weight':'bold','opacity':'1'});
         break;
       case '2':
         $('#progress-bar').val('34');
         $progressBarComp.find('div.first').addClass('border-change');
-        $progressBarComp.find('div.second').addClass('border-change');
+        $progressBarComp.find('div.second').addClass('border-change').find('p').css({'font-weight':'bold','opacity':'1'});
         break;
       case '3':
         $('#progress-bar').val('67');
         $progressBarComp.find('div.first').addClass('border-change');
         $progressBarComp.find('div.second').addClass('border-change');
-        $progressBarComp.find('div.third').addClass('border-change');
+        $progressBarComp.find('div.third').addClass('border-change').find('p').css({'font-weight':'bold','opacity':'1'});
         break;
       case '4':
         $('#progress-bar').val('100');
         $progressBarComp.find('div.first').addClass('border-change');
         $progressBarComp.find('div.second').addClass('border-change');
         $progressBarComp.find('div.third').addClass('border-change');
-        $progressBarComp.find('div.fourth').addClass('border-change');
+        $progressBarComp.find('div.fourth').addClass('border-change').find('p').css({'font-weight':'bold','opacity':'1'});
         break;
       default:
         $('#progress-bar').val('34');
         $progressBarComp.find('div.first').addClass('border-change');
-        $progressBarComp.find('div.second').addClass('border-change');
+        $progressBarComp.find('div.second').addClass('border-change').find('p').css({'font-weight':'bold','opacity':'1'});
         break;  
     }
   }else{
     $('#progress-bar').val('34');
     $progressBarComp.find('div.first').addClass('border-change');
-    $progressBarComp.find('div.second').addClass('border-change');
+    $progressBarComp.find('div.second').addClass('border-change').find('p').css({'font-weight':'bold','opacity':'1'});
   }
 });
