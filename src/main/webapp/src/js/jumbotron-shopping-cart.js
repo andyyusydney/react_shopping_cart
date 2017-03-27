e160e2d5dd37364cb55df067e2fd7e134b733bbe
FOXTEL\zangli <lin.zang@foxtel.com.au>
2017-03-27 17:26:50 +1100

    PRF-143: Update cart js

--- src/main/webapp/src/js/jumbotron-shopping-cart.js
+++
@@ -1,9 +0,0 @@
-/* Foxtel now jumbotron shopping cart */
-
-$(document).ready(function(){
-	if($('.foxtel-jumbotron--shopping-cart').find('.foxtel-jumbotron__pack-tag')){
-		$('.foxtel-jumbotron__pack-tag').on('click',function(){
-			$(this).remove();
-		})
-	}
-})
