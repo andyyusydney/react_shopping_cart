4e9ed2a1cbd9f7b4c11eecf47fc0ffd735a87f12
zhengf <frank.zheng@foxtel.com.au>
2017-04-04 08:25:47 +1000

    epl rules

--- src/main/webapp/src/js/shop-cart.js
+++ src/main/webapp/src/js/shop-cart.js
@@ -12,23 +12,60 @@ com.foxtel.ShopCartManager = function() {
     var MODEL_NAME_FOR_CART_SERVLET ="modelShopCart";
     var CART_SERVLET_URL = "/bin/foxtel/now/cart";
 
+    var EPL_CHANNEL_TIERS = [
+        {
+            name:"Chelsea TV",
+            tierIdWithoutSports:991139
+            tierIdWithSports:991145
+        },
+        {
+            name:"Liverpool TV",
+            tierIdWithoutSports:991140
+            tierIdWithSports:991146
+        },
+        {
+            name:"Manchest TV",
+            tierIdWithoutSports:991141
+            tierIdWithSports:991147
+        },
+    ];
+
+    var sport_tier_id = 990703;
+
     function init(){
         //shop cart servlet response
         this.shopCartResponseData = null;
         var self = this;
 
+        //keep a copy in client side
+        //we need to change the request to follow EPL rules
+        self.current_play_tiers = [];
+
         FOX.dyc.subscribeEvent(MODEL_NAME_FOR_CART_SERVLET,function(data){
             self.shopCartResponseData = data;
             FOX.context.broadcast("SHOP_CART_LOADED",data);
         });
+
+        //init the 3 epl channle ids
+        self.epl_channel_with_sport_ids = [];
+        self.epl_channel_without_sport_ids = [];
+        $.each(EPL_CHANNEL_TIERS,function(idx,item){
+            self.epl_channel_with_sport_ids.push(item.tierIdWithSports);
+            self.epl_channel_without_sport_ids.push(item.tierIdWithoutSports);
+        })
     }
 
     function addPlayTier(tierId,callback){
         var self = this;
 
-        var tierIds = self.getCurrentPlayTiers();
-        tierIds = _.union(tierIds, [tierId]);
-        self.updatePlayTiers(tierIds,callback);
+        var tierIdsAdded = [tierId];
+        //add 3 epl free channels when adding sports
+        if(tierId == sport_tier_id){
+            tierIdsAdded = _.union(tierIdsAdded,self.epl_channel_with_sport_ids);
+        }
+
+        //call
+        self.addPlayTiers(tierIdsAdded,callback);
     }
 
     function addPlayTiers(tierIdsAdded,callback){
@@ -97,6 +134,7 @@ com.foxtel.ShopCartManager = function() {
     return {
         init:init,
         addPlayTier:addPlayTier,
+        addPlayTiers:addPlayTiers,
         removePlayTier:removePlayTier,
         updatePlayTiers:updatePlayTiers,
         getCurrentPlayTiers:getCurrentPlayTiers
