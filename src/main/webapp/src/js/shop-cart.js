144d1580692f2e126efd45be5fa773388bf2be15
FOXTEL\zangli <lin.zang@foxtel.com.au>
2017-04-04 12:11:51 +1000

    Update shopping cart filter EPL  channels

--- src/main/webapp/src/js/shop-cart.js
+++ src/main/webapp/src/js/shop-cart.js
@@ -32,10 +32,21 @@ com.foxtel.ShopCartManager = function() {
 
     var sport_tier_id = 990703;
 
+    function getEPLTiers(){
+        return this.EPL_CHANNEL_TIERS;
+    }
+
+    function getSportTierId(){
+        return this.sport_tier_id;
+    }
+
     function init(){
         //shop cart servlet response
         this.shopCartResponseData = null;
         var self = this;
+
+        self.EPL_CHANNEL_TIERS = EPL_CHANNEL_TIERS;
+        self.sport_tier_id = sport_tier_id;
 
         //keep a copy in client side
         //we need to change the request to follow EPL rules
@@ -150,7 +161,9 @@ com.foxtel.ShopCartManager = function() {
         removePlayTier:removePlayTier,
         removePlayTiers:removePlayTiers,
         updatePlayTiers:updatePlayTiers,
-        getCurrentPlayTiers:getCurrentPlayTiers
+        getCurrentPlayTiers:getCurrentPlayTiers,
+        getEPLTiers:getEPLTiers,
+        getSportTierId:getSportTierId
     }
 
 };
