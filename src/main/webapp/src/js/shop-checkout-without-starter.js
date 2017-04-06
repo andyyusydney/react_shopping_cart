a51e038258171502f6bb83242c8658d79ae53213
zhengf <frank.zheng@foxtel.com.au>
2017-04-06 14:45:25 +1000

    new component

---
+++ src/main/webapp/src/js/shop-checkout-without-starter.js
@@ -0,0 +1,57 @@
+
+/**
+ * User story link :https://confluence.aws.foxtel.com.au/pages/viewpage.action?pageId=46077595
+ * JIRA : PRF-22
+ */
+
+
+$(document).ready(function(){
+    var $cmp = $("[data-cmp-id='checkoutWithoutStarterPack']");
+    if($cmp.length == 0){
+        return;
+    }
+
+    var modelData = FOX.context.getModel("checkoutWithoutStarterPackModel");
+
+    //Update title Text when having cart object
+    FOX.context.subscribe("SHOP_CART_LOADED",function(cartResponse){
+        var titleText = modelData.title;
+
+        //get premium tier names
+        var premiumTierNames = [];
+        $.each(cartResponse.play.tiers,function(idx,item){
+            premiumTierNames.push(item.title);
+        })
+        var replaceText = "";
+        var len = premiumTierNames.length;
+        for(var idx;idx<len;idx++){
+            replaceText += premiumTierNames[idx];
+            if(idx< len -2){
+                replaceText += ",";
+            }
+            if(idx == len -2){
+                replaceText += "&";
+            }
+        }
+        titleText = titleText.replace("[selected_tier_names]");
+        titleText = titleText.replace("[selected_tier_names]");
+        if(len==1){
+            titleText = titleText.replace("[be]","is");
+        }else{
+            titleText = titleText.replace("[be]","are");
+        }
+
+        $cmp.find("[data-id='title']").html(titleText);
+    });
+
+    //click event handler
+    $cmp.on('click',"[data-id='moreOptionsBtn']",function(){
+        Foxtel.navigator(modelData.moreOptionsBtnLink);
+    });
+
+    $cmp.on('click',"[data-id='addStarterBtn']",function(){
+        Foxtel.ShopCartManager.addPlayTier(modelData.starterTierId,function(){
+            Foxtel.navigator(modelData.moreOptionsBtnLink);
+        });
+    });
+});
