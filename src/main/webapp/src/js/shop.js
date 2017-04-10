557fbdb3a33ab73d24c9fad207b2a3ad31cbd313
Daniel Mort <dan@asithappens.io>
2017-04-11 09:36:37 +1000

    Reuse ShopCartManager#hasPremiumPackAndNoStarter in shop.js.

--- src/main/webapp/src/js/shop.js
+++ src/main/webapp/src/js/shop.js
@@ -137,30 +137,11 @@ $(document).ready(function(){
 
     //Navigation
     $(document).on('click','[data-button-url]',function(){
-        $this = $(this);
-
-        //check if it's empty
-        var cartResponse = Foxtel.ShopCartManager.getCartResponse();
-        if(cartResponse.play.tiers.length == 0){
-          //TODO disabled the button
-          return;
-        }
-
-        //do we have starter pack
-        var hasStarterPack = false;
-        for(var i = 0; i< cartResponse.play.tiers.length;i++){
-          if ('GENRE' == cartResponse.play.tiers[i].type) {
-            hasStarterPack = true;
-            break;
-          }
-        }
-
-        if(hasStarterPack){
-            Foxtel.navigator($this.data("button-url"));
-        }else{
+        if(Foxtel.ShopCartManager.hasPremiumPackAndNoStarter()){
             Foxtel.navigator($this.data("button-without-starter-url"));
+        }else{
+            Foxtel.navigator($this.data("button-url"));
         }
-
     });
 
 
