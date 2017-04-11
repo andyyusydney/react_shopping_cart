fda0e72d5aace7994ba8133a1650aa7dd10dfcc9
zhengf <frank.zheng@foxtel.com.au>
2017-04-11 17:47:31 +1000

    merge from foxtel-ui

--- node-build/webpack.config.js
+++ node-build/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 var jsonFile = require('jsonfile');
+var httpProxy = require('http-proxy');
+var http = require('http');
+
 
 module.exports = {
   context: path.resolve(__dirname, '../src/main/webapp/src/js'),
@@ -21,17 +24,54 @@ module.exports = {
     contentBase: path.resolve(__dirname, './dist'),
     port:9000,
     setup: function(app) {
-
       app.post('/*', function (req, res) {
-         console.log("===="+req.originalUrl);
-
          var outputFolder = path.resolve(__dirname, './dist');
          var outputFile = path.join(outputFolder,req.originalUrl);
          console.log(jsonFile.readFileSync(outputFile));
          res.send(jsonFile.readFileSync(outputFile));
-        //res.redirect("http://127.0.0.1:9000/"+req.originalUrl);
       });
 
     },
   }
 };
+
+//proxy server
+
+var proxy = httpProxy.createProxyServer({});
+
+var assetServer = "http://127.0.0.1:9000";
+var aemServer = "http://127.0.0.1:4502";
+
+var mappingRules = [
+];
+
+var server = http.createServer(function(req, res) {
+
+  var url = req.url;
+
+  function gotoAssetServer(req,res){
+    proxy.web(req, res, {
+      target: assetServer
+    });
+  }
+
+  //foxtel now js and css
+  if( /\etc\/designs.*(shop*js)/.test(url)){
+    gotoAssetServer(req,res);
+    return;
+  }
+
+  //ajax calls
+  if( /\\bin/.test(url)){
+    gotoAssetServer(req,res);
+    return;
+  }
+
+  //by default,goes to aem
+  proxy.web(req, res, {
+    target: aemServer
+  });
+});
+
+console.log("listening on port 5050")
+server.listen(80);
