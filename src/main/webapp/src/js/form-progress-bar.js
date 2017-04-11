7a4995e3965c732139f82ce6417ad261801dd734
zhengf <frank.zheng@foxtel.com.au>
2017-04-11 11:11:41 +1000

    PRF-388 navigation bar link

--- src/main/webapp/src/js/form-progress-bar.js
+++ src/main/webapp/src/js/form-progress-bar.js
@@ -10,33 +10,6 @@ $(document).ready(function(){
     return;
   }
 
-  // //set default value
-  // $progressBarComp.on('click','.foxtel-now-progress-bar-dot-wrapper',function(){
-  //   $this = $(this);
-  //   var dataLink = $this.data('status-link');
-
-  //   if ($(this).hasClass('first')){
-
-  //       $('#progress-bar').val('0');
-  //       $(this).nextAll().removeClass('border-change');
-  //      }else if ($(this).hasClass('second')){
-  //       $(this).nextAll().removeClass('border-change');  
-  //       $('#progress-bar').val('34');
-  //       $(this).prevAll().addClass('border-change');  
-  //       $(this).addClass('border-change');
-  //      }else if ($(this).hasClass('third')){
-  //       $(this).nextAll().removeClass('border-change');  
-  //       $('#progress-bar').val('67');
-  //       $(this).prevAll().addClass('border-change'); 
-  //       $(this).addClass('border-change');
-  //      } else{
-  //        $('#progress-bar').val('100');
-  //        $(this).addClass('border-change');
-  //       $(this).prevAll().addClass('border-change');
-
-  //      }
-  // });
-
   //detect data-default-step and set status value when page load
   var dataDefaultStep = $progressBarComp.attr('data-default-step');
 
@@ -76,4 +49,12 @@ $(document).ready(function(){
     $progressBarComp.find('div.first').addClass('border-change');
     $progressBarComp.find('div.second').addClass('border-change').find('p').css({'font-weight':'bold','opacity':'1'});
   }
+
+  //click event
+  $progressBarComp.on('click',".foxtel-now-progress-bar-dot-wrapper",function(){
+        var href = $(this).data('status-link');
+        if(href && href.length>0){
+            Foxtel.navigator(href);
+        }
+  })
 });
