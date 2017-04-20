45faa6c044022eb5b23e574351932668b51e99f5
zhengf <frank.zheng@foxtel.com.au>
2017-04-21 09:36:45 +1000

    node build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
+
 const path = require('path');
-var jsonFile = require('jsonfile');
-var http = require('http');
-var httpProxy = require('http-proxy');
 var glob = require("glob");
 
 module.exports = {
@@ -11,64 +9,5 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist/js'),
     filename: '[name].js',
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
-    port:9000,
-    setup: function(app) {
-
-      app.post('/*', function (req, res) {
-         console.log("Retrieving mocked response for :"+req.originalUrl);
-
-         var outputFolder = path.resolve(__dirname, './dist');
-         var outputFile = path.join(outputFolder,req.originalUrl);
-         res.send(jsonFile.readFileSync(outputFile));
-      });
-
-    },
   }
 };
-
-
-var proxy = httpProxy.createProxyServer();
-
-
-var LOCAL_AEM_SERVER = "http://127.0.0.1:4502";
-var LOCAL_WEBPACK_SERVER = "http://127.0.0.1:9000";
-
-/**
- * This is main access point
- *
- */
-http.createServer(function (req, res) {
-
-    var url = req.url;
-
-    if(/.*foxtel-main-ui.js/.test(url)){
-        req.url = 'foxtelmainui.js';
-        proxy.web(req, res, {
-          target: LOCAL_WEBPACK_SERVER
-        });
-        return;
-    }
-
-    if(/\/bin\/foxtel\/now.*/.test(url)){
-        proxy.web(req, res, {
-           target: LOCAL_WEBPACK_SERVER
-        });
-        return;
-    }
-
-    if(/\/bin\/secure\.*/.test(url)){
-        proxy.web(req, res, {
-           target: LOCAL_WEBPACK_SERVER
-        });
-        return;
-    }
-
-    req.url = req.url+"?wcmmode=disabled";
-    proxy.web(req, res, {
-      target: LOCAL_AEM_SERVER
-    });
-
-});
