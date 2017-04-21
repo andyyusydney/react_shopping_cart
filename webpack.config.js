952cad0e3189f1da04dbb414eba4e4697d5dcdc4
zhengf <frank.zheng@foxtel.com.au>
2017-04-21 11:07:51 +1000

    node build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,38 @@
 
 const path = require('path');
 var glob = require("glob");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+var stylishReporter = require('jshint-loader-stylish')({
+});
 
 module.exports = {
-  entry: {
-    mainui: glob.sync('./src/main/webapp/src/js/*')
-  },
-  output: {
-    path: path.resolve(__dirname, './dist/js'),
-    filename: '[name].js',
-  }
+    entry: {
+        mainui: glob.sync('./src/main/webapp/src/js/*')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist/js'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use :[
+                    {
+                        loader: 'jshint-loader',
+                        options: {
+                            emitErrors: false,
+                            failOnHint: true,
+                            reporter : stylishReporter
+                        }
+                    },
+                ]
+
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist.js'],{verbose:  true})
+    ]
 };
