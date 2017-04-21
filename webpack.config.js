7ce36fd93ccf42e2ea838adf9396517f1e49f225
Daniel Mort <dan@asithappens.io>
2017-04-21 15:12:09 +1000

    Bundle main styles in node build.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var jsonFile = require('jsonfile');
 var http = require('http');
 var httpProxy = require('http-proxy');
 var glob = require("glob");
+var webpack = require("webpack");
 
 module.exports = {
   entry: {
@@ -11,6 +12,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
+    publicPath: '/'
   },
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
@@ -26,7 +28,33 @@ module.exports = {
       });
 
     },
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {}
+        }
+      }
+    ]
+  },
+  plugins: [
+    // enable HMR globally
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
 
 
@@ -52,6 +80,19 @@ http.createServer(function (req, res) {
         return;
     }
 
+    if(/.*shop-checkout.js/.test(url)){
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("");
+        return;
+    }
+
+    if(/.*foxtel-main-ui.css/.test(url)){
+        // Main css will be bundled with js.
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("Blank\n");
+        return;
+    }
+
     if(/\/bin\/foxtel\/now.*/.test(url)){
         proxy.web(req, res, {
            target: LOCAL_WEBPACK_SERVER
@@ -62,6 +103,13 @@ http.createServer(function (req, res) {
     if(/\/bin\/secure\.*/.test(url)){
         proxy.web(req, res, {
            target: LOCAL_WEBPACK_SERVER
+        });
+        return;
+    }
+
+    if(/.*hot-update.json/.test(url)){
+        proxy.web(req, res, {
+          target: LOCAL_WEBPACK_SERVER
         });
         return;
     }
