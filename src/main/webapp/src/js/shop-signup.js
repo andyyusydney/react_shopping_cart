51219504ed7eaa6bde9c697b476e6d1ef58fdf9d
zhengf <frank.zheng@foxtel.com.au>
2017-04-26 11:30:16 +1000

    PRF-539 fixed

--- src/main/webapp/src/js/shop-signup.js
+++ src/main/webapp/src/js/shop-signup.js
@@ -151,15 +151,17 @@ $(document).ready(function(){
         });
     }
 
-    $form = $("#reactivation-personal-details-form");
-    if($form.length > 0){
-        hasForm = true;
-        new com.foxtel.now.ReactivationCustomerDetailsForm().config({
-            submitBtnSelector:"#sign-up-form-submit",
-            $form:$form,
-            submitAjaxUrl:"/bin/foxtel/now/my-account/reactivation/customerDetails",
-            loadAjaxUrl:"/bin/foxtel/now/my-account/reactivation/customerDetailLanding"
-        });
+    if(!hasForm){
+        $form = $("#reactivation-personal-details-form");
+        if($form.length > 0){
+            hasForm = true;
+            new com.foxtel.now.ReactivationCustomerDetailsForm().config({
+                submitBtnSelector:"#sign-up-form-submit",
+                $form:$form,
+                submitAjaxUrl:"/bin/foxtel/now/my-account/reactivation/customerDetails",
+                loadAjaxUrl:"/bin/foxtel/now/my-account/reactivation/customerDetailLanding"
+            });
+        }
     }
 
     //stop loading if there is no form
