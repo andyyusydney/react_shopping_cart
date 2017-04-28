b104d967b54624135cfee915a06242188f817be0
zhengf <frank.zheng@foxtel.com.au>
2017-04-28 14:03:00 +1000

    PRF-562 frontend fixes

--- src/main/webapp/src/js/my-profile-edit.js
+++ src/main/webapp/src/js/my-profile-edit.js
@@ -206,6 +206,9 @@ $(function () {
 
       updateKenan: function (formData) {
         var payload = {
+          firstName:formData.firstName,
+          lastName:formData.lastName,
+          dataOfBirth:formData.dateOfBirth.replace(/\//g,'-'),
           custEmail: formData.email,
           dayPhone: formData.mobile,
           billAddressOne: formData.address,
