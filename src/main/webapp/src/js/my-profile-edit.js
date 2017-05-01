7e694a9b196842c3069314bbad712613579f153b
Daniel Mort <dan@asithappens.io>
2017-05-01 14:40:31 +1000

    PRF-550 Updates dummy address.

--- src/main/webapp/src/js/my-profile-edit.js
+++ src/main/webapp/src/js/my-profile-edit.js
@@ -179,7 +179,7 @@ $(function () {
         // Hide default addresses
         var defaultAddresses = [
           '5 THOMAS HOLT DRIVE',
-          '1 Foxtel now road'
+          '1 FOXTEL NOW ROAD'
         ];
         if (_(defaultAddresses).contains(response.kBillAddress1)) {
           response.kBillAddress1 = "";
