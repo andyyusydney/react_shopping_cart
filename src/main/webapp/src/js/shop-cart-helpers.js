4db8047817cece880abd295938848bcc5295826c
James Zhou <james.q.zhou@foxtel.com.au>
2017-05-05 00:04:52 +1000

    fix creditcard issue

--- src/main/webapp/src/js/shop-cart-helpers.js
+++ src/main/webapp/src/js/shop-cart-helpers.js
@@ -199,8 +199,10 @@ Handlebars.registerHelper("shopCartTimeConverter",function(cartResponse,options)
 
 Handlebars.registerHelper("shopCartStringToDateConverter",function(cartResponse,options) {
 
-  var months = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06', 'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'};
-  var dateStr = cartResponse.split(" ");
+ // var months = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06', 'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dec':'12'};
+    var months = {'January':'01', 'February':'02', 'March':'03', 'April':'04', 'May':'05', 'June':'06', 'July':'07', 'August':'08', 'September':'09', 'October':'10', 'November':'11', 'December':'12'};
+
+    var dateStr = cartResponse.split(" ");
   var yearStr = dateStr[1];
   var monthStr = dateStr[0];
   var year = yearStr.slice(-2);
